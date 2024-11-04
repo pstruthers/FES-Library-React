5590a19c1e996fb28a6b9b61d56e4851b0354973
Paige <paige.struthers@gmail.com>
2024-11-03 22:08:51 -0600

    complete app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,72 @@
-import Nav from "./components/Nav.jsx"
-import Landing from './components/Landing.jsx'
-import Highlights from "./components/Highlights.jsx";
-import Featured from "./components/Featured.jsx";
-import Discounted from "./components/Discounted.jsx";
-import Explore from "./components/Explore.jsx";
+import Nav from "./components/Nav.jsx";
 import Footer from "./components/Footer.jsx";
+import Home from "./pages/Home.jsx";
+import Books from "./pages/Books.jsx";
+import { books } from "./data";
+import BookInfo from "./pages/BookInfo.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Cart from "./pages/Cart.jsx";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  function addToCart(book) {
+    setCart([...cart, { ...book, quantity: 1 }]);
+  }
+
+  function changeQuantity(book, quantity) {
+    setCart(
+      cart.map((item) =>
+        item.id === book.id
+          ? {
+              ...item,
+              quantity: +quantity,
+            }
+          : item
+      )
+    );
+  }
+
+  function removeBook(item) {
+    setCart(cart.filter(book => book.id !== item.id))
+  }
+
+  function numberOfItems() {
+    let counter = 0;
+    cart.forEach(item => {
+      counter += item.quantity
+    })
+    return counter;
+  }
+
+  useEffect(() => {
+    console.log(cart);
+  }, [cart]);
+
   return (
-    <div className="App">
-      <Nav />
-      <Landing />
-      <Highlights />
-      <Featured />
-      <Discounted />
-      <Explore />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Nav numberOfItems={numberOfItems()} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/books" element={<Books books={books} />} />
+          <Route
+            path="/books/:id"
+            element={
+              <BookInfo books={books} addToCart={addToCart} cart={cart} />
+            }
+          />
+          <Route
+            path="/cart"
+            element={
+              <Cart books={books} cart={cart} changeQuantity={changeQuantity} removeBook={removeBook} />
+            }
+          />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -1,39 +1,54 @@
-import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Rating from "./Rating";
+import Price from "./Price";
 
 const Book = ({ book }) => {
+  const [img, setImg] = useState();
+
+  useEffect(() => {
+    const image = new Image();
+    image.src = book.url;
+    image.onload = () => {
+      setTimeout(() => {
+        setImg(image);
+      }, 1000)
+    }
+  })
+
   return (
     <div className="book">
-      <a href="">
-        <figure className="book__img--wrapper">
-          <img src={book.url} alt="" className="book__img" />
-        </figure>
-      </a>
-      <div className="book__title">
-        <a href="/" className="book__title--link">
-          {book.title}
-        </a>
-      </div>
-      <div className="book__ratings">
-        {new Array(Math.floor(book.rating)).fill(0).map((_, index) => (
-          <FontAwesomeIcon icon="star" key={index} />
-        ))}
-        {
-            !Number.isInteger(book.rating) && <FontAwesomeIcon icon="star-half-alt" />
-        }
-      </div>
-      <div className="book__price">
-        {book.salePrice ? (
-          <>
-            <span className="book__price--normal">
-              ${book.originalPrice.toFixed(2)}
-            </span>
-            ${book.salePrice.toFixed(2)}
-          </>
-        ) : (
-          <>${book.originalPrice.toFixed(2)}</>
-        )}
-      </div>
+      {img ? (
+        <>
+          <Link to={`/books/${book.id}`}>
+            <figure className="book__img--wrapper">
+              <img
+                src={img.src}
+                alt=""
+                className="book__img"
+              />
+            </figure>
+          </Link>
+          <div className="book__title">
+            <Link to={`/books/${book.id}`} className="book__title--link">
+              {book.title}
+            </Link>
+          </div>
+          <Rating rating={book.rating} />
+          <Price
+            originalPrice={book.originalPrice}
+            salePrice={book.salePrice}
+          />{" "}
+        </>
+      ) : (
+        <>
+          {" "}
+          <div className="book__img--skeleton"></div>
+          <div className="skeleton book__title--skeleton"></div>
+          <div className="skeleton book__rating--skeleton"></div>
+          <div className="skeleton book__price--skeleton"></div>
+        </>
+      )}
     </div>
   );
 };
